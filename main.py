2e4fa54904260db70615417d61649f6680a3112f
RomanKiado-12 <820418@mystma.org>
2022-09-16 14:22:04 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+    return a +" " + b + " " + c + " " + d + " " + e
 topFiveMovies("Shrek", "shrek 2", "Shrek 3", "Shrek 4", "Shrek 5")
 
